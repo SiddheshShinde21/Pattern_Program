64de5257e06d74e71815caa0078e2a1ab3b934fc
Siddhesh Shinde <103132924+SiddheshShinde21@users.noreply.github.com>
2024-01-13 20:39:34 +0530

    Create Mirrored Right Triangle Star Pattern.java

---
+++ Mirrored Right Triangle Star Pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class LearnYard {
+    public static void main(String[] args) {
+        // you code goes here
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		for(int row = 1;row<=N;row++){
+			for(int sp = 1;sp<=N-row;sp++){
+				System.out.print(" ");
+			}
+			for(int str = 1;str<=row;str++){
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+    }
+}
